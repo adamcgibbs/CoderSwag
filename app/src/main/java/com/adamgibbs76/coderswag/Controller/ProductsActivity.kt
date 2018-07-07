19b0a80ebbeed03d7a9bcc1a26fad447bfad4093
adamgibbs <adamcgibbs76@gmail.com>
2018-07-07 16:17:03 +0930

    Lesson 08

--- app/src/main/java/com/adamgibbs76/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/adamgibbs76/coderswag/Controller/ProductsActivity.kt
@@ -1,18 +1,39 @@
 package com.adamgibbs76.coderswag.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.adamgibbs76.coderswag.Adapters.ProductsAdapter
 import com.adamgibbs76.coderswag.R
+import com.adamgibbs76.coderswag.Services.DataService
 import com.adamgibbs76.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
 
-        println(categoryType)
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720){
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
     }
 }
