becb515d576b7e464feaa0ac2ce34f64dfb7e92b
adamgibbs <adamcgibbs76@gmail.com>
2018-07-07 15:07:16 +0930

    Lesson 05 - Refactored for RecyclerView adapter

--- app/src/main/java/com/adamgibbs76/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/adamgibbs76/coderswag/Adapters/CategoryAdapter.kt
@@ -10,10 +10,7 @@ import android.widget.TextView
 import com.adamgibbs76.coderswag.Model.Category
 import com.adamgibbs76.coderswag.R
 
-class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter(){
-
-    val context = context
-    val categories = categories
+class CategoryAdapter(val context: Context, val categories: List<Category>) : BaseAdapter(){
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView: View
--- app/src/main/java/com/adamgibbs76/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/adamgibbs76/coderswag/Controller/MainActivity.kt
@@ -2,9 +2,11 @@ package com.adamgibbs76.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.adamgibbs76.coderswag.Adapters.CategoryAdapter
+import com.adamgibbs76.coderswag.Adapters.CategoryRecycleAdapter
 import com.adamgibbs76.coderswag.Model.Category
 import com.adamgibbs76.coderswag.R
 import com.adamgibbs76.coderswag.Services.DataService
@@ -12,15 +14,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService. categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
 
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
 
     }
 }
