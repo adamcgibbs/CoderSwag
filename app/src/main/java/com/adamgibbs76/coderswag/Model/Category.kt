c6effdbc3d01f57a00e2a48f86d04f6d98e27d55
adamgibbs <adamcgibbs76@gmail.com>
2018-07-07 14:27:47 +0930

    Lesson 04

--- app/src/main/java/com/adamgibbs76/coderswag/Model/Category.kt
+++ app/src/main/java/com/adamgibbs76/coderswag/Model/Category.kt
@@ -1,6 +1,6 @@
 package com.adamgibbs76.coderswag.Model
 
-class Category(val title: String, val imaage: String) {
+class Category(val title: String, val image: String) {
     override fun toString(): String {
         return title
     }
